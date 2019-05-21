4c20a083f031c86fa4e1d9e871e8875031fb3eef
whosAle <abelgrave95@gmail.com>
2019-05-21 15:11:10 -0400

    aversion feature complete

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 import SushiWallet from './components/SushiWallet';
+// import AversionForm from './components/AversionForm';
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
@@ -41,14 +42,29 @@ class App extends Component {
   }
 
   handleMoreButtonClick = () => {
-    const newSushiIndex = this.state.sushiIndex + 4 >= this.state.sushis.length ? 0 : this.state.sushiIndex + 4
+    //Before Aversions feature
+    // const newSushiIndex = this.state.sushiIndex + 4 >= this.state.sushis.length ? 0 : this.state.sushiIndex + 4
+    // this.setState({sushiIndex: newSushiIndex})
+
+    let newSushiIndex;
+    if (this.state.aversions.length > 0) {
+      let filteredSushis = this.state.sushis.filter(sushi => !this.state.aversions.includes(sushi.name))
+      newSushiIndex = this.state.sushiIndex + 4 >= filteredSushis.length ? 0 : this.state.sushiIndex + 4
+    } else {
+      newSushiIndex = this.state.sushiIndex + 4 >= this.state.sushis.length ? 0 : this.state.sushiIndex + 4
+    }
     this.setState({sushiIndex: newSushiIndex})
+
   }
 
   handleWalletSubmit = (value) => {
     // console.log(value);
     // event.preventDefault();
     this.setState({budget: this.state.budget + parseInt(value)})
+  }
+
+  handleAversionSubmit = (aversion) => {
+    this.setState({aversions: [...this.state.aversions, aversion]});
   }
 
   //End Event Handlers
@@ -59,16 +75,34 @@ class App extends Component {
   render() {
     // console.log("sushiIndex is:", this.state.sushiIndex);
 
+    /*//Old SushisonDisplay before aversion feature
     //generates the 4 sushis that will be displayed on the belt
-    const sushisOnDisplay = this.state.sushis.slice(this.state.sushiIndex, this.state.sushiIndex+4)
+    // const sushisOnDisplay = this.state.sushis.slice(this.state.sushiIndex, this.state.sushiIndex+4)*/
+
+    let sushisOnDisplay = [];
+    if (this.state.aversions.length === 0) {
+      //generates the 4 sushis that will be displayed on the belt
+      sushisOnDisplay = this.state.sushis.slice(this.state.sushiIndex, this.state.sushiIndex+4)
+    } else {
+      const filteredSushis = this.state.sushis.filter(sushi => !this.state.aversions.includes(sushi.name))
+      console.log(filteredSushis);
+      sushisOnDisplay = filteredSushis.slice(this.state.sushiIndex, this.state.sushiIndex+4)
+    }
+
+
 
     //generates the sushis that have been eaten.
     const eatenSushis = this.state.sushis.filter(sushi => sushi.eaten)
+
+    //generates the unique sushi names by mapping all the names then grabbing
+    //the unique values
+    const sushiNames = [...new Set(this.state.sushis.map(sushi => sushi.name))]
     return (
       <div className="app">
         <SushiContainer sushis={sushisOnDisplay} onSushiClick={this.handleSushiClick} onMoreButtonClick={this.handleMoreButtonClick} />
         <Table budget={this.state.budget} bill={this.state.bill} eatenSushis={eatenSushis} onWalletSubmit={this.handleWalletSubmit}/>
-        <SushiWallet onWalletSubmit={this.onWalletSubmit} />
+        <SushiWallet onWalletSubmit={this.onWalletSubmit} sushiNames={sushiNames} onAversionSubmit={this.handleAversionSubmit} />
+        {/* <AversionForm onAversionSubmit{this.handleAversionSubmit}/> */ null}
       </div>
     );
   }
--- sushi-saga-client/src/components/SushiWallet.js
+++ sushi-saga-client/src/components/SushiWallet.js
@@ -7,17 +7,26 @@ const SushiWallet = (props) => {
     props.onWalletSubmit(e.target.amount.value);
   }
 
+  const handleAversion = (e) => {
+    // debugger
+    e.preventDefault();
+    props.onAversionSubmit(e.target.aversion.value);
+  }
+
   return (
-    <div class="wallet">
+    <div className="wallet">
       <h3>Add More To Your Wallet!</h3>
       <form onSubmit={handleSubmit}>
         <label name="Amount"/> Enter Amount to Add!:
         <input type="number" placeholder="15" name="amount"/>
         <input type="submit"/>
       </form>
-      <form onSubmit={(e) => e.preventDefault()}>
+
+      <form onSubmit={handleAversion}>
         <label name="aversion"/> Enter Any Sushi Aversions:
-        <input type="number" placeholder="" name="aversion"/>
+        <select name="aversion">
+          {props.sushiNames.map(name => <option>{name}</option>)}
+        </select>
         <input type="submit"/>
       </form>
 
